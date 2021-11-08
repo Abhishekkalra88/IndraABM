bfb011b39606e384fdc3ea127522268c138182c7
lmenezesl <luiza.menezes@nyu.edu>
2021-11-08 18:13:45 -0500

    documentation

--- APIServer/model_manager.py
+++ APIServer/model_manager.py
@@ -9,18 +9,20 @@ from db.model_db import get_model_by_id
 modelManager = None
 
 class ModelProcessAttrs:
+    #New model is being created, once the endpoint is done we call spawn_model within ModelManager
     def __init__(self, process, parent_conn, model_id):
-        self.process = process
+        self.process = process 
         self.parent_conn = parent_conn
         self.model_id = model_id
 
 class ModelManager:
     def __init__(self):
         print("Creating new model manager")
-        self.processes = LRU(cpu_count() * 5 + 1)       # Not too many processes but also not too less
+        self.processes = LRU(cpu_count() * 5 + 1) # Not too many processes but also not too little
+        #If we ahve too many processes, spawn will then use LRU to get 'rid' of them.
 
-    def get_model(self, exec_key):
-        return self.processes[exec_key]
+    def get_model(self, exec_key): 
+        return self.processes[exec_key] #Every model has a unique execution key that it can be idenfitied with
     
     def to_json(self):
         ret_json = {}
@@ -28,9 +30,11 @@ class ModelManager:
             ret_json[key] = get_model_by_id(val.model_id).name
         return ret_json
 
+    #First step to occur after model is initialized, when we start the server we don't have child processes until we get requests
+    #model_id is used as a username for that model to identify it
     def spawn_model(self, model_id, payload, indra_dir):
-        parent_conn, child_conn = Pipe()
-        new_process = Process(target=createModelProcess, args=(child_conn, model_id, payload, indra_dir))
+        parent_conn, child_conn = Pipe() #we use pipe to communicate between parent and child; child process runs the actual model
+        new_process = Process(target=createModelProcess, args=(child_conn, model_id, payload, indra_dir)) #each model runs in a process of its own
         mp = ModelProcessAttrs(new_process, parent_conn, model_id)
         new_process.start()
         model = parent_conn.recv()
@@ -38,7 +42,7 @@ class ModelManager:
         return model
 
     def run_model(self, exec_key, runtime):
-        modelProcess = self.processes[exec_key]
+        modelProcess = self.processes[exec_key] #Uses the child process
         if(modelProcess is None):
             return None
         message = Message(CommunicationType.RUN_MODEL, {'runtime': runtime})
--- APIServer/model_process.py
+++ APIServer/model_process.py
@@ -13,9 +13,10 @@ class Message:
     self.data = data
 
 # This function is run in each process that is created for a model
+# This is called within the target from the spawn_model function
 def createModelProcess(conn, model_id, payload, indra_dir):
-    model = create_model(model_id, payload, indra_dir)
-    conn.send(model)                            # The first time a process is created, it send back the model was created
+    model = create_model(model_id, payload, indra_dir) #This uses the child process
+    conn.send(model)  # The first time a process is created, it send back the model was created
     while True:                                 # The process then goes into an infinite loop listening on the pipe
         message = conn.recv()
         if message.type == CommunicationType.RUN_MODEL:
